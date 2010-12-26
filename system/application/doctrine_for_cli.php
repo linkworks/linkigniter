9b4389f15fbb64b6214536752bc3ed3f97ac6d91
Ian Murray <ianmurrays@gmail.com>
2010-12-26 00:33:39 -0300

    Modified doctrine_for_cli.php so it creates docrtine_table classes too

--- system/application/doctrine_for_cli.php
+++ system/application/doctrine_for_cli.php
@@ -15,7 +15,36 @@ $config = array('data_fixtures_path'  =>  dirname(__FILE__) . DIRECTORY_SEPARATO
                 'models_path'         =>  dirname(__FILE__) . DIRECTORY_SEPARATOR . '/models',
                 'migrations_path'     =>  dirname(__FILE__) . DIRECTORY_SEPARATOR . '/migrations',
                 'sql_path'            =>  dirname(__FILE__) . DIRECTORY_SEPARATOR . '/sql',
-                'yaml_schema_path'    =>  dirname(__FILE__) . DIRECTORY_SEPARATOR . '/schema');
+                'yaml_schema_path'    =>  dirname(__FILE__) . DIRECTORY_SEPARATOR . '/schema',
+                );
+
+$config['generate_models_options'] = array(
+  // Define the PHPDoc Block in the generated classes
+  'phpDocPackage'         =>'LinkIgniter',
+  'phpDocSubpackage'      =>'Models',
+  'phpDocName'            =>'YOUR_NAME_HERE',
+  'phpDocEmail'           =>'YOUR@NAME.HERE',
+  //'phpDocVersion'         =>'1.0',
+  
+  // Define whats what and named how, where.
+  'suffix'                => '.php',
+  'pearStyle'             => TRUE,
+  'baseClassPrefix'       => 'Base_',
+  
+  // Unless you have created a custom class or want Default_Model_Base_Abstract
+  'baseClassName'         => 'Doctrine_Record',
+  
+  // Leave this empty as specifying 'Base' will create Base/Base
+  'baseClassesDirectory'  => NULL,
+  
+  // Should make it Zend Framework friendly AFAIK
+  //'classPrefix'           => 'Dagho_Model_',
+  'classPrefixFiles'      => FALSE,
+  'generateBaseClasses'   => TRUE,
+  'generateTableClasses'  => TRUE,
+  //'packagesPath'          => APPLICATION_PATH . '/models',
+  //'packagesFolderName'    => 'packages',
+);
 
 $cli = new Doctrine_Cli($config);
 $cli->run($_SERVER['argv']);
